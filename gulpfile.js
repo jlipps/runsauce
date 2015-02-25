ad9d69b1ef416ac13c9e314d5fe9ba1201209a5e
Jonathan Lipps <jlipps@gmail.com>
2015-02-24 17:07:10 -0800

    get source maps working

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,23 @@
 
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var sourcemaps = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var insert = require("gulp-insert-lines");
 
 gulp.task("default", function () {
+  var mapPath = null;
   return gulp.src("src/*.js")
-    .pipe(babel())
+    .pipe(rename(function (path) {
+      path.basename = path.basename.replace(".es6", "");
+      mapPath = path.basename + ".map";
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(babel({experimental: true, optional: ["runtime"], sourceMap: "inline"}))
+    .pipe(insert({
+      after: /"use strict";/,
+      lineAfter: "require('source-map-support').install();\n"
+    }))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
 });
--- src/main.es6.js
+++ src/main.es6.js
@@ -1,48 +1,55 @@
 "use strict";
 
-var runner = require('./runner.js')
-  , _ = require('underscore')
-  , opts = require('./parser.js').parse()
-  , testsMap = require('./parser.js').testsMap
-  , setup = require('./setup.js')
-  , monocle = require('monocle-js');
+//import _ from 'lodash';
+//import { parser, testsMap } from './parser';
+//import { run } from './runner';
+//import { setup } from './setup';
 
-monocle.run(function*() {
-  if (_.has(opts, 'setup') && opts.setup) {
-    yield setup.interactiveSetup();
-    process.exit(0);
-  }
-  var config = setup.getConfig();
-  if (config === null) {
-    console.error("Could not load config file, please run with --setup");
-    process.exit(1);
-  }
-  if (!_.has(config, opts.config)) {
-    console.error("Config " + opts.config + " doesn't exist");
-    process.exit(1);
-  }
-  if (!_.contains(_.keys(testsMap), opts.test)) {
-    console.error("Test type '" + opts.test + "' is not valid, run --tests");
-    process.exit(1);
-  }
-  var caps = {
-    browserName: opts.browser
-    , device: opts.device
-    , version: opts.version.toString()
-    , platform: opts.platform
-    , name: testsMap[opts.test]
-  };
-  if (opts.orientation) {
-    caps['device-orientation'] = opts.orientation;
-  }
-  yield runner.run(_.extend({
-    testType: opts.test
-    , wait: opts.wait
-    , localname: opts.localname
-    , configName: opts.config
-    , runs: opts.runs
-    , framework: opts.framework
-    , processes: opts.processes
-    , backendVersion: opts.backendVersion
-  }, config[opts.config]), caps);
+//const opts = parser.parse();
+
+async function main () {
+  throw new Error('foo');
+  //if (_.has(opts, 'setup') && opts.setup) {
+    //yield setup.interactiveSetup();
+    //process.exit(0);
+  //}
+  //var config = setup.getConfig();
+  //if (config === null) {
+    //console.error("Could not load config file, please run with --setup");
+    //process.exit(1);
+  //}
+  //if (!_.has(config, opts.config)) {
+    //console.error("Config " + opts.config + " doesn't exist");
+    //process.exit(1);
+  //}
+  //if (!_.contains(_.keys(testsMap), opts.test)) {
+    //console.error("Test type '" + opts.test + "' is not valid, run --tests");
+    //process.exit(1);
+  //}
+  //var caps = {
+    //browserName: opts.browser
+    //, device: opts.device
+    //, version: opts.version.toString()
+    //, platform: opts.platform
+    //, name: testsMap[opts.test]
+  //};
+  //if (opts.orientation) {
+    //caps['device-orientation'] = opts.orientation;
+  //}
+  //yield runner.run(_.extend({
+    //testType: opts.test
+    //, wait: opts.wait
+    //, localname: opts.localname
+    //, configName: opts.config
+    //, runs: opts.runs
+    //, framework: opts.framework
+    //, processes: opts.processes
+    //, backendVersion: opts.backendVersion
+  //}, config[opts.config]), caps);
+}
+
+main().then(console.log, err => {
+  console.error(err.stack);
+  process.exit(1);
 });
+
