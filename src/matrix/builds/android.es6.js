5fa06f1e04d28334927b48a2ad2a46f579d35ccc
Jonathan Lipps <jlipps@gmail.com>
2015-04-27 12:20:08 -0500

    fix up android matrix

--- src/matrix/builds/android.es6.js
+++ src/matrix/builds/android.es6.js
@@ -2,23 +2,17 @@ let opts = {c: 'prod', u: 'appium-android-matrix-%t', n: 20,
             name: "Appium Android support matrix"};
 let androidWebTestOpts = {};
 let androidTestOpts = {};
-let appiumVers = ['1.0.0', '1.1.0', '1.2.0', '1.2.1', '1.2.2',
-                  '1.2.4', '1.3.1', '1.3.3', '1.3.4', '1.3.6', '1.3.7-beta'];
+let appiumVers = ['1.0.0', '1.1.0', '1.2.4', '1.3.6', '1.3.7'];
 androidTestOpts.a = androidWebTestOpts.a = appiumVers;
 androidTestOpts.r = 1;
 let androidVers = ['2.3', '4.0', '4.1', '4.2', '4.3', '4.4', '5.0'];
 androidWebTestOpts.v = androidTestOpts.v = androidVers;
-//androidWebTestOpts.a = androidTestOpts.a = appiumVers;
-androidWebTestOpts.a = androidTestOpts.a = ['1.3.6', '1.3.7-beta'];
 androidWebTestOpts.t = ['web_guinea|v>=4.4', 'selfsigned|v>=4.4', 'connect|v>=4.4'];
 androidWebTestOpts.d = ['ae', 'Samsung Galaxy S4 Device|v=4.4', 'Samsung Galaxy S5 Device|v=4.4'];
-androidWebTestOpts.b = ['b', 'c|d!=ae'];
+androidWebTestOpts.b = ['b|d=ae', 'c|d!=ae'];
 androidTestOpts.t = ['android|v>=4.2', 'android_hybrid|v>=4.4', 'selendroid'];
-//androidTestOpts.t = ['android|v>=4.2'];
-//androidTestOpts.d = ['ae', 'Samsung Galaxy S4 Emulator', 'Samsung Galaxy S4 Device|v=4.3',
-                     //'Samsung Galaxy S4 Device|v=4.4', 'Samsung Galaxy S5 Device|v=4.4'];
-androidTestOpts.d = ['ae', 'Samsung Galaxy S4 Emulator', 'Samsung Galaxy S4 Device|v=4.4'];
+androidTestOpts.d = ['ae', 'Samsung Galaxy S4 Emulator', 'Samsung Galaxy S4 Device|v=4.3',
+                     'Samsung Galaxy S4 Device|v=4.4', 'Samsung Galaxy S5 Device|v=4.4'];
 opts.tests = [androidTestOpts, androidWebTestOpts];
-//opts.tests = [androidTestOpts];
 
 export default opts;
