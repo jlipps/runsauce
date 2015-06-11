aa166b1b977844dc10d1d8d4181bfc2e75bfd4b4
Jonathan Lipps <jlipps@gmail.com>
2015-06-11 12:07:33 -0700

    modify appium ios build

--- src/matrix/builds/ios.es6.js
+++ src/matrix/builds/ios.es6.js
@@ -2,15 +2,15 @@ let opts = {c: 'prod', u: 'appium-ios-matrix-%t', n: 20,
             name: 'Appium iOS support matrix'};
 let basicTestOpts = {};
 let deviceTestOpts = {};
-let appiumVers = ['1.0.0', '1.1.0', '1.2.4', '1.3.6', '1.3.7', '1.4.0-beta'];
+let appiumVers = ['1.0.0', '1.1.0', '1.2.4', '1.3.6', '1.3.7', '1.4.0'];
 let iosVers = ['6.1', '7.0', '7.1', '8.0|a>=1.3.1', '8.1|a>=1.3.1',
                '8.2|a>=1.3.6'];
 basicTestOpts.a = deviceTestOpts.a = appiumVers;
-basicTestOpts.r = deviceTestOpts.r = 1;
+basicTestOpts.r = deviceTestOpts.r = 5;
 basicTestOpts.v = deviceTestOpts.v = iosVers;
-basicTestOpts.t = ['ios', 'web_guinea', 'selfsigned', 'connect', 'ios_loc_serv'];
+//basicTestOpts.t = ['ios', 'web_guinea', 'selfsigned', 'connect', 'ios_loc_serv'];
+basicTestOpts.t = deviceTestOpts.t = ['ios', 'web_guinea', 'ios_loc_serv'];
 basicTestOpts.d = ['ip', 'ipa'];
-deviceTestOpts.t = ['web_guinea', 'ios_loc_serv'];
 deviceTestOpts.d = ['iPhone Retina (3.5-inch)|v=7.0', 'iPhone 5s|v=7.1',
                     'iPad 2|v=7.1', 'iPhone 6 Plus|v>=8.0',
                     'iPad Air|v>=8.0'];
