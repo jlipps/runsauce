be584a4d21e68b334c4d75db39f06ce1acfe57da
Scott Dixon <scott@howrefreshing.com.au>
2016-06-13 16:08:53 -0700

    Sumo Logic collection endpoint docs

--- src/utils.es6.js
+++ src/utils.es6.js
@@ -1,13 +1,11 @@
 import Sumologic from 'logs-to-sumologic';
 
 export function sendToSumo (url, logs) {
-  const sumologic = Sumologic.createClient({
-    url: url
-  });
+  const sumologic = Sumologic.createClient({url});
   return new Promise(function(resolve, reject) {
     sumologic.log(logs, function (err) {
       if (err) {
-        reject(err);
+        return reject(err);
       }
       resolve();
     });
