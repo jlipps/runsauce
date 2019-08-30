455ee558b04fa11c9c335b5977f5f0f15981a447
Isaac Murchie <isaac.appium@gmail.com>
2019-08-30 14:09:34 -0400

    chore: update deps and run eslint

--- test/parser-specs.js
+++ test/parser-specs.js
@@ -3,24 +3,24 @@ const chai = require('chai');
 
 chai.should();
 
-describe('parser', () => {
-  describe('mapArgs', () => {
-    it('should convert shortcuts in args to actual values', () => {
+describe('parser', function () {
+  describe('mapArgs', function () {
+    it('should convert shortcuts in args to actual values', function () {
       mapArgs({b: 's'}).should.eql({browser: 'Safari'});
       mapArgs({p: 'm9'}).should.eql({platform: 'Mac 10.9'});
       mapArgs({o: 'l'}).should.eql({orientation: 'landscape'});
     });
-    it('should convert shortcuts for the whole arg set', () => {
+    it('should convert shortcuts for the whole arg set', function () {
       mapArgs({b: 's', p: 'm9'}).should.eql({browser: 'Safari', platform: 'Mac 10.9'});
     });
-    it('should convert shortcuts even when the param is not a shortcut', () => {
+    it('should convert shortcuts even when the param is not a shortcut', function () {
       mapArgs({browser: 's', platform: 'm9'})
         .should.eql({browser: 'Safari', platform: 'Mac 10.9'});
     });
-    it('should prefer full keys to shortcut keys', () => {
+    it('should prefer full keys to shortcut keys', function () {
       mapArgs({browser: 's', b: 'c'}).should.eql({browser: 'Safari'});
     });
-    it('should not convert values for params that dont have shortcuts', () => {
+    it('should not convert values for params that dont have shortcuts', function () {
       mapArgs({browser: 's', a: '1.6.5'})
         .should.eql({browser: 'Safari', backendVersion: '1.6.5'});
     });
